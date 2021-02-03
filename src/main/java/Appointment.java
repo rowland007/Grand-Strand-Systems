4611a6b4704855c15a3559c7e4dcbcc52ced5ac5
Randy <1226496+rowland007@users.noreply.github.com>
2021-02-02 23:28:20 -0700

    Add Feature/Appointment (#26)
    * Add Feature/Appointment
    
    Meets requirements for issue #13
    
    * Add appointment date methods
    
    Meets requirements for issue #14
    
    * Add appointment description
    
    Meets requirements for issue #15
    
    Created initializer for constructors and removed dead code.
    
    * Add new appointment service
    
    Meets requirements for issue #16
    
    * Add delete appointment service
    
    Meets requirements for issue #17
    
    * Add unit tests
    
    * Update access to allow tests
    
    * Restyle Add Feature/appointment (#27)
    
    * Restyled by clang-format
    
    * Restyled by whitespace
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

---
+++ src/main/java/Appointment.java
@@ -0,0 +1,85 @@
+import java.util.Date;
+
+public class Appointment {
+
+  final private byte APPOINTMENT_ID_LENGTH;
+  final private byte APPOINTMENT_DESCRIPTION_LENGTH;
+  final private String INITIALIZER;
+  private String appointmentId;
+  private Date appointmentDate;
+  private String description;
+
+  {
+    APPOINTMENT_ID_LENGTH = 10;
+    APPOINTMENT_DESCRIPTION_LENGTH = 50;
+    INITIALIZER = "INITIAL";
+  }
+
+  Appointment() {
+    Date today = new Date();
+    appointmentId = INITIALIZER;
+    appointmentDate = today;
+    description = INITIALIZER;
+  }
+
+  Appointment(String id) {
+    Date today = new Date();
+    updateAppointmentId(id);
+    appointmentDate = today;
+    description = INITIALIZER;
+  }
+
+  Appointment(String id, Date date) {
+    updateAppointmentId(id);
+    updateDate(date);
+    description = INITIALIZER;
+  }
+
+  Appointment(String id, Date date, String description) {
+    updateAppointmentId(id);
+    updateDate(date);
+    updateDescription(description);
+  }
+
+  public void updateAppointmentId(String id) {
+    if (id == null) {
+      throw new IllegalArgumentException("Appointment ID cannot be null.");
+    } else if (id.length() > APPOINTMENT_ID_LENGTH) {
+      throw new IllegalArgumentException("Appointment ID cannot exceed " +
+                                         APPOINTMENT_ID_LENGTH +
+                                         " characters.");
+    } else {
+      this.appointmentId = id;
+    }
+  }
+
+  public String getAppointmentId() { return appointmentId; }
+
+  public void updateDate(Date date) {
+    if (date == null) {
+      throw new IllegalArgumentException("Appointment date cannot be null.");
+    } else if (date.before(new Date())) {
+      throw new IllegalArgumentException(
+          "Cannot make appointment in the past.");
+    } else {
+      this.appointmentDate = date;
+    }
+  }
+
+  public Date getAppointmentDate() { return appointmentDate; }
+
+  public void updateDescription(String description) {
+    if (description == null) {
+      throw new IllegalArgumentException(
+          "Appointment description cannot be null.");
+    } else if (description.length() > APPOINTMENT_DESCRIPTION_LENGTH) {
+      throw new IllegalArgumentException(
+          "Appointment description cannot exceed " +
+          APPOINTMENT_DESCRIPTION_LENGTH + " characters.");
+    } else {
+      this.description = description;
+    }
+  }
+
+  public String getDescription() { return description; }
+}
---
+++ src/main/java/AppointmentService.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+public class AppointmentService {
+  final private List<Appointment> appointmentList = new ArrayList<>();
+
+  private String newUniqueId() {
+    return UUID.randomUUID().toString().substring(
+        0, Math.min(toString().length(), 10));
+  }
+
+  public void newAppointment() {
+    Appointment appt = new Appointment(newUniqueId());
+    appointmentList.add(appt);
+  }
+
+  public void newAppointment(Date date) {
+    Appointment appt = new Appointment(newUniqueId(), date);
+    appointmentList.add(appt);
+  }
+
+  public void newAppointment(Date date, String description) {
+    Appointment appt = new Appointment(newUniqueId(), date, description);
+    appointmentList.add(appt);
+  }
+
+  public void deleteAppointment(String id) throws Exception {
+    appointmentList.remove(searchForAppointment(id));
+  }
+
+  protected List<Appointment> getAppointmentList() { return appointmentList; }
+
+  private Appointment searchForAppointment(String id) throws Exception {
+    int index = 0;
+    while (index < appointmentList.size()) {
+      if (id.equals(appointmentList.get(index).getAppointmentId())) {
+        return appointmentList.get(index);
+      }
+      index++;
+    }
+    throw new Exception("The appointment does not exist!");
+  }
+}
